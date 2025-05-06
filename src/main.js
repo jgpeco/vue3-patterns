e84eb932b10f2594773bd2734e33a124fec134f9
jgpeco <joaogabrielonofre@gmail.com>
2025-05-06 16:45:20 -0300

    feat: create modal plugin to inject in app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Modals from './plugins/modals'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(Modals)
+  .mount('#app')

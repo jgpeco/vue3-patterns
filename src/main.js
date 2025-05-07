dc0a239141f48fe93c0266e1bc4f7c587054da26
jgpeco <joaogabrielonofre@gmail.com>
2025-05-07 15:00:20 -0300

    refactor: move styles to assets folder and adjust styles file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import Modals from './plugins/modals'
+import { createApp } from "vue"
+import App from "./App.vue"
+import Modals from "./plugins/modals"
+import styles from "./assets/styles.css"
 
 createApp(App)
-  .use(Modals)
-  .mount('#app')
+    .use(Modals)
+    .mount("#app")

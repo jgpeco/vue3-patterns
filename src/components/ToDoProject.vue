2f33a1bfd2824fcc0181bf603d36866558e9e328
jgpeco <joaogabrielonofre@gmail.com>
2025-05-07 14:51:14 -0300

    feat: create header and container for todolist

---
+++ src/components/ToDoProject.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="project-container">
+    Oi
+  </div>
+</template>
+
+<style scoped>
+  .project-container {
+    max-width: 56rem;
+    padding: 1rem;
+    margin: 0 auto;
+  }
+</style>

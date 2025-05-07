2a4ef22699ee30320642cc6b10bbe6c63435eb57
jgpeco <joaogabrielonofre@gmail.com>
2025-05-07 16:01:49 -0300

    feat: create filter input

--- src/components/TodoFilter.vue
+++ src/components/TodoFilter.vue
@@ -1,8 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const
+  $props = defineProps(['modelValue']),
+  $emit = defineEmits(['update:modelValue']),
+  _value = ref('')
+
+onMounted(() => {
+  _value.text = $props.modelValue
+})
+
+function clear() {
+  _value.value = '',
+  publishValue()
+}
+
+function publishValue() {
+  $emit('update:modelValue', _value.value)
+}
+
+</script>
 
 <template>
-  <div>
-    TodoFilter
+  <div class="flex-container w3-border">
+    <input 
+      type="text"
+      class="w3-input w3-border-0"
+      v-model="_value"
+      @keyup="publishValue()"
+      placeholder="Enter filter text..."
+    >
+    <button class="w3-border-0" @click="clear()">
+      <i class="fas fa-fw fa-times"></i>
+    </button>
   </div>
 </template>
 
